0a596c729a8a5acadc92f3dad59f82d4671ce89d
Edenilson <freitaseddie126@gmail.com>
2025-05-19 20:17:55 -0300

    project created

---
+++ src/main/java/com/devsuperior/dslist03/Dslist03Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dslist03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dslist03Application.class, args);
+	}
+
+}
